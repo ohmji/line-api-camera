ec73b0156eeb91a2c107223f6a7051c904158fb6
ohmji <ohmji@localhost.localdomain>
2019-12-13 00:12:41 +0700

    update redirect

--- src/constants/auth.constants.js
+++ src/constants/auth.constants.js
@@ -2,5 +2,5 @@ export const authConstants = {
     LINE_CHANNEL_SECRET:"tEABV8o+Yy85QSzsQx5bsnz3MB5hLb8wfx4LXe701BuCDJLgbj3GWjj6ADdIcy1b5A9wUiE41HYfSAv4PS85XLoEDNJ5fgKl1Ig0S88HMG1i2gujRxC2+woT0viJdjDKc+HDMKgrK9w/7/OWqahgWwdB04t89/1O/w1cDnyilFU=",
     LINE_CHANNEL_ID:"1653643445",
     LINE_CHANNEL_SECRET:"a2c04a79a36bdc5aa28670308459ca99",
-    LINE_CALL_BACK:"https://camera-api-message.netlify.com/confirm"
+    LINE_CALL_BACK:"http://localhost:8080/confirm"
 };
